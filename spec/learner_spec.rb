849378c9844e9b2fe3080777577d7fa3d68533ac
Andrew Liu <andrewliu33@gmail.com>
2011-10-11 13:43:12 +0800

    Update spec

--- spec/learner_spec.rb
+++ spec/learner_spec.rb
@@ -32,4 +32,21 @@ describe "Learner" do
       learner.update_count.should > 0
     end
   end
+
+  describe "Adpative Perceptron Learner" do
+    it "should run adaptive perceptron learning in hyperspace" do
+      learner = ML::Learner::AdaptivePerceptronLearner.new(4, 0.1)
+
+      generator = ML::Data::Generator.new(4)
+      data = generator.points(10, ML::Data::Generator.generate_vector(4))
+
+      learner.train! data
+
+      line = learner.line
+      line.should.kind_of?(Array).should == true
+      line.size.should == 5
+
+      learner.update_count.should > 0
+    end
+  end
 end
